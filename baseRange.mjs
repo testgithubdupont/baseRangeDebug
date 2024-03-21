a946d5ebd7eb93ce7814eb1c671d643852143c7d
Super Dupont <denis.dupont@univ-evry.fr>
2024-03-21 17:36:06 +0100

    add script start

--- baseRange.mjs
+++ baseRange.mjs
@@ -4,19 +4,11 @@ const printArg = Debug('baseRange:arg')
     , printTab = Debug('baseRange:tab');
 
 export default function baseRange(...arg) {
-
-
     let [start, end, step = 1, fromRight = false] = arg;
-
-
     let index = -1
         , length = Math.max(Math.ceil((end - start) / (step)), 0)
         , result = Array(length);
-
-
     printArg(start, end, step, result.length);
-
-
     while (length--) {
         result[fromRight ? length : ++index] = start;
         start += step;
